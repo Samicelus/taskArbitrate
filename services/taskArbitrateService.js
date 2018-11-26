4102fc6d8403f1165f4f8c8cf7cbd0a766ae3bfe
samicelus <samicelus@hotmail.com>
2018-11-27 03:49:29 +0800

    commit logs

--- services/taskArbitrateService.js
+++ services/taskArbitrateService.js
@@ -1,23 +1,20 @@
 const path = require('path');
 const mongoose = require('mongoose');
-
 const Task = require('../models/task.js');
 const Machine = require('../models/machine.js');
-
 const BaseHandler = require(path.join(__dirname, '../libs/baseHandler.js'));
-
 let handlers = new BaseHandler();
 
 // 当任务完成时调用, 当任务被分配到机器上 Task.times 之后调用
 async function onTaskDone(task) {
-    console.log(`task: ${task.id} done in: ${task.times} sec, free cpu num for machine: ${task.machineId}`);
+    //console.log(`task: ${task.id} done in: ${task.times} sec, free cpu num for machine: ${task.machineId}`);
     //释放cpu数
     let machineId = task.machineId;
     let machineObj = await Machine.schema.findOne({id:machineId}).exec();
     machineObj.freeCpus += task.cpus;
     machineObj.usedCpus -= task.cpus;
     await machineObj.save();
-    console.log(`machine ${machineObj.id} free cpus num is :${machineObj.freeCpus}`);
+    //console.log(`machine ${machineObj.id} free cpus num is :${machineObj.freeCpus}`);
 }
 
 // 当需要任务开始执行时调用, 返回当前可执行该任务的 Machine, 如没有满足条件的 Machine 则返回 null
@@ -44,79 +41,9 @@ async function onTaskSchedule(task){
         }
     }
     if(temp_result != null){
-        console.log(`running task: ${task.id} on machine: ${temp_result.id} ...`);
+        //console.log(`running task: ${task.id} on machine: ${temp_result.id} ...`);
     }
     return temp_result;
-}
-
-async function runSimulation(logger) {
-    //提取task，并按id排序
-    console.log(`getting tasks...`);
-    let condition = {
-        machineId: {"$exists": false}
-    };
-    let tasks = await Task.schema.find(condition).sort({id: 1}).exec();
-    logger.push(`found ${tasks.length} tasks...`);
-    await arbitrateSequence(tasks, logger)
-}
-
-//递归执行机器分配,直至task全部分配一次
-async function arbitrateSequence(tasks, logger){
-    if(tasks.length > 0){
-        //给task 分配 machine
-        let current_task = tasks.shift();
-        let machine = await onTaskSchedule(current_task);
-
-        //执行
-        if(machine != null){
-            machine.freeCpus -= current_task.cpus;
-            machine.usedCpus += current_task.cpus;
-            await machine.save();
-            logger.push(`run task ${current_task.id} on machine ${machine.id}, cpu: ${machine.freeCpus}/${machine.cpus}`);
-            current_task.machineId = machine.id;
-            await current_task.save();
-        }else{
-            logger.push(`no machine scheduled for task ${current_task.id}`);
-        }
-        await arbitrateSequence(tasks, logger);
-    }else{
-       await simulateTaskRun(logger);
-    }
-}
-
-//模拟任务进行
-async function simulateTaskRun(logger){
-    //查询分配到的任务
-    let condition = {
-        machineId: {"$exists": true},
-        timeLeft: {"$gt": 0}
-    };
-    let runnningTasks = await Task.schema.find(condition).sort({timeLeft: 1}).exec();
-    if(runnningTasks.length > 0){
-        let timeDecrease = runnningTasks[0].timeLeft;
-
-        logger.push(`less time task ${runnningTasks[0].id}, time left: ${timeDecrease} sec ...`)
-        let finishCondition = {
-            machineId: {"$exists": true},
-            timeLeft: timeDecrease
-        };
-        let finishedTasks = await Task.schema.find(finishCondition).exec();
-
-        //消耗时间
-        await Task.schema.update(condition,{"$inc":{"timeLeft":-timeDecrease}}, {"multi":true});
-        logger.push(`time passed by ${timeDecrease} sec ...`);
-
-        //触发每个任务完成
-        for(let task of finishedTasks){
-            logger.push(`task ${task.id} done, free ${task.cpus} cpus for machine ${task.machineId}`);
-            await onTaskDone(task);
-        }
-
-        //再次执行任务分配
-        await runSimulation(logger);
-    }else{
-        logger.push(`no more task to run, end arbitration`);
-    }
 }
 
 /**
@@ -156,13 +83,84 @@ handlers.addTestTasks = async function(ctx, next) {
 };
 
 /**
- * @samicelus 测试分配任务
+ * @samicelus 测试执行
  **/
 handlers.runTest = async function(ctx, next) {
     let logger = [];
     await runSimulation(logger);
     handlers.restSuccess(ctx, logger);
 };
+
+
+async function runSimulation(logger) {
+    //提取task，并按id排序
+    console.log(`getting tasks...`);
+    let condition = {
+        machineId: {"$exists": false}
+    };
+    let tasks = await Task.schema.find(condition).sort({id: 1}).exec();
+    logger.push(`found ${tasks.length} tasks...`);
+    await arbitrateSequence(tasks, logger)
+}
+
+//递归执行机器分配,直至task全部分配一次
+async function arbitrateSequence(tasks, logger){
+    if(tasks.length > 0){
+        //给task 分配 machine
+        let current_task = tasks.shift();
+        let machine = await onTaskSchedule(current_task);
+
+        //执行
+        if(machine != null){
+            machine.freeCpus -= current_task.cpus;
+            machine.usedCpus += current_task.cpus;
+            await machine.save();
+            logger.push(`run task ${current_task.id} on machine ${machine.id}, cpu: ${machine.freeCpus}/${machine.cpus}`);
+            current_task.machineId = machine.id;
+            await current_task.save();
+        }else{
+            logger.push(`no machine scheduled for task ${current_task.id}`);
+        }
+        await arbitrateSequence(tasks, logger);
+    }else{
+        await simulateTaskRun(logger);
+    }
+}
+
+//模拟任务进行
+async function simulateTaskRun(logger){
+    //查询分配到的任务
+    let condition = {
+        machineId: {"$exists": true},
+        timeLeft: {"$gt": 0}
+    };
+    let runnningTasks = await Task.schema.find(condition).sort({timeLeft: 1}).exec();
+    if(runnningTasks.length > 0){
+        let timeDecrease = runnningTasks[0].timeLeft;
+
+        logger.push(`less time task ${runnningTasks[0].id}, time left: ${timeDecrease} sec ...`)
+        let finishCondition = {
+            machineId: {"$exists": true},
+            timeLeft: timeDecrease
+        };
+        let finishedTasks = await Task.schema.find(finishCondition).exec();
+
+        //消耗时间
+        await Task.schema.update(condition,{"$inc":{"timeLeft":-timeDecrease}}, {"multi":true});
+        logger.push(`time passed by ${timeDecrease} sec ...`);
+
+        //触发每个任务完成
+        for(let task of finishedTasks){
+            logger.push(`task ${task.id} done, free ${task.cpus} cpus for machine ${task.machineId}`);
+            await onTaskDone(task);
+        }
+
+        //再次执行任务分配
+        await runSimulation(logger);
+    }else{
+        logger.push(`no more task to run, end arbitration`);
+    }
+}
 
 /**
  * @samicelus 分步测试
